20436b58f3e9deb33175427bee76d90498997beb
R35 <R35@DESKTOP-C4F52MO>
2020-02-24 11:00:10 +0900

    Root Application Config - DI

---
+++ guestbook03/src/main/java/com/douzone/guestbook/repository/GuestbookRepository.java
@@ -0,0 +1,167 @@
+package com.douzone.guestbook.repository;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import com.douzone.guestbook.vo.GuestbookVo;
+
+@Repository
+public class GuestbookRepository {
+	public int delete( GuestbookVo vo ) {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = 
+				" delete" + 
+				"   from guestbook" + 
+				"  where no=?" +
+				"    and password=?";
+			pstmt = conn.prepareStatement( sql );
+
+			pstmt.setLong( 1, vo.getNo() );
+			pstmt.setString( 2, vo.getPassword() );
+
+			count = pstmt.executeUpdate();
+		} catch (SQLException e) {
+			System.out.println("error :" + e);
+		} finally {
+			// 자원 정리
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return count;		
+	}
+	
+	public int insert(GuestbookVo vo) {
+		int count = 0;
+		Connection conn = null;
+		PreparedStatement pstmt = null;
+
+		try {
+			conn = getConnection();
+
+			String sql = 
+				" insert" + 
+				"   into guestbook(name, password, contents, reg_date)" + 
+				" values (?, ?, ?, now() )";
+			pstmt = conn.prepareStatement(sql);
+
+			pstmt.setString(1, vo.getName());
+			pstmt.setString(2, vo.getPassword());
+			pstmt.setString(3, vo.getContents());
+
+			count = pstmt.executeUpdate();
+
+		} catch (SQLException e) {
+			System.out.println("error :" + e);
+		} finally {
+			// 자원 정리
+			try {
+				if (pstmt != null) {
+					pstmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return count;
+	}
+
+	public List<GuestbookVo> findAll() {
+		List<GuestbookVo> list = new ArrayList<GuestbookVo>();
+
+		Connection conn = null;
+		Statement stmt = null;
+		ResultSet rs = null;
+
+		try {
+			conn = getConnection();
+
+			// Statement 객체 생성
+			stmt = conn.createStatement();
+
+			// SQL문 실행
+			String sql =
+				"   select no," + 
+				"          name," + 
+				"	       contents," + 
+				"     	   date_format(reg_date, '%Y-%m-%d %h:%i:%s')" + 
+				"     from guestbook" + 
+				" order by reg_date desc";
+			rs = stmt.executeQuery( sql );
+
+			// 결과 가져오기(사용하기)
+			while (rs.next()) {
+				Long no = rs.getLong(1);
+				String name = rs.getString(2);
+				String contents = rs.getString(3);
+				String regDate = rs.getString(4);
+
+				GuestbookVo vo = new GuestbookVo();
+				vo.setNo(no);
+				vo.setName(name);
+				vo.setContents( contents );
+				vo.setRegDate( regDate );
+
+				list.add(vo);
+			}
+		} catch (SQLException e) {
+			System.out.println("error :" + e);
+		} finally {
+			// 자원 정리
+			try {
+				if (rs != null) {
+					rs.close();
+				}
+				if (stmt != null) {
+					stmt.close();
+				}
+				if (conn != null) {
+					conn.close();
+				}
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return list;
+	}
+
+	private Connection getConnection() throws SQLException {
+		Connection conn = null;
+		try {
+			Class.forName("org.mariadb.jdbc.Driver");
+			String url = "jdbc:mysql://192.168.1.100:3307/webdb";
+			conn = DriverManager.getConnection(url, "webdb", "webdb");
+		} catch( ClassNotFoundException e ) {
+			System.out.println( "드러이버 로딩 실패:" + e );
+		} 
+		
+		return conn;
+	}	
+}
