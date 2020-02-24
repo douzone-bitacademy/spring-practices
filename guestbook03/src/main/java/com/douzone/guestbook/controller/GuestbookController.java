eafb04d27c16e4fd758061baabb3345dab8d2b96
R35 <R35@DESKTOP-C4F52MO>
2020-02-24 10:16:36 +0900

    JSP 정리

--- guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
+++ guestbook03/src/main/java/com/douzone/guestbook/controller/GuestbookController.java
@@ -2,14 +2,12 @@ package com.douzone.guestbook.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class GuestbookController {
 
-	@ResponseBody
 	@RequestMapping("")
 	public String index() {
-		return "guestbook list";
+		return "/WEB-INF/views/index.jsp";
 	}
 }
