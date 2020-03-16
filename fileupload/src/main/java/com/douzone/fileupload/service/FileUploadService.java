ffe20a45136dac5e33ad72fa349d742036a65461
R35 <R35@DESKTOP-C4F52MO>
2020-03-16 14:25:46 +0900

    fielupload 구현#1

---
+++ fileupload/src/main/java/com/douzone/fileupload/service/FileUploadService.java
@@ -0,0 +1,11 @@
+package com.douzone.fileupload.service;
+
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+@Service
+public class FileUploadService {
+	public void restore(MultipartFile multipartFile) {
+		
+	}
+}
