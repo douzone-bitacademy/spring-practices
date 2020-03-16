294aea50eb53a6f45af197405be024719ae417f9
R35 <R35@DESKTOP-C4F52MO>
2020-03-16 15:53:02 +0900

    fileupload 구현 final

--- fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
+++ fileupload/src/main/java/com/douzone/fileupload/controller/FileUploadController.java
@@ -2,6 +2,7 @@ package com.douzone.fileupload.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,10 +24,13 @@ public class FileUploadController {
 	@RequestMapping(value="upload", method=RequestMethod.POST)
 	public String upload(
 		@RequestParam(value="email", required=true, defaultValue="") String email,
-		@RequestParam(value="upload-file") MultipartFile multipartFile) {
+		@RequestParam(value="upload-file") MultipartFile multipartFile,
+		Model model) {
 		System.out.println("email:" + email);
 		
-		fileUploadService.restore(multipartFile);
+		String url = fileUploadService.restore(multipartFile);
+
+		model.addAttribute("url", url);
 		return "result";
 	}
 }
