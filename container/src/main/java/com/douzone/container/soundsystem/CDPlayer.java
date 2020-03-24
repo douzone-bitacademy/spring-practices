c2884a846d3576c0630892eb8c1a5aedb443a230
R35 <R35@DESKTOP-C4F52MO>
2020-03-24 10:00:59 +0900

    명시적 설정 - Java Config #1

--- container/src/main/java/com/douzone/container/soundsystem/CDPlayer.java
+++ container/src/main/java/com/douzone/container/soundsystem/CDPlayer.java
@@ -1,15 +1,14 @@
 package com.douzone.container.soundsystem;
 
-import javax.inject.Inject;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CDPlayer {
 	// Wiring01: 바이너리 주입(CGLib) 
-	// @Inject (자바 표준 Wiring 어노테이션)
-	// @Autowired (스프링 Wiring 어노테이션)
+	// @Inject //(자바 표준 Wiring 어노테이션)
+	@Autowired //(스프링 Wiring 어노테이션)
 	@Qualifier("highSchoolRapper3Final")
 	private CompactDisc cd;
 
